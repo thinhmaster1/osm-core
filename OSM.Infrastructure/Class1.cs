40646e4c1ab6be9dd2c332543e3efda58ece7170
thinhmaster1 <buidacthinh.wakemeup@gmail.com>
2018-07-10 11:57:39 +0700

    build solution structure

--- OSM.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OSM.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ OSM.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSM.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+    }
+}
